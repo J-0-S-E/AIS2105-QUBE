eab4e72f1e1e6eac8bc1ef6008dc9b5cd7b99a4d
kristian <kristian.lokkeberg@gmail.com>
2026-03-12 10:43:22 +0100

    Oppgave 1 - complete

---
+++ src/qube_description/setup.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'qube_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+        ['resource/qube_description']),
+        ('share/qube_description', ['package.xml']),
+        (os.path.join('share', 'qube_description', 'launch'),
+        glob('launch/*.launch.py')),
+        (os.path.join('share', 'qube_description', 'urdf'),
+        glob('urdf/*.xacro')),
+        (os.path.join('share', 'qube_description', 'config'),
+        glob('config/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kristian',
+    maintainer_email='kristian.lokkeberg@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
