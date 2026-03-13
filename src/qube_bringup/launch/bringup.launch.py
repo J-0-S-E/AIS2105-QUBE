cb05f434f10e7bae7d97019507dbbb9150b92a5d
armancv1 <arman.c.valseth@gmail.com>
2026-03-13 09:18:06 +0100

    Oppgave 3

---
+++ src/qube_bringup/launch/bringup.launch.py
@@ -0,0 +1,45 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    qube_driver_pkg = get_package_share_directory('qube_driver')
+    qube_bringup_pkg = get_package_share_directory('qube_bringup')
+
+    # Prosesser xacro-filen til URDF
+    urdf = xacro.process_file(
+        os.path.join(qube_bringup_pkg, 'urdf', 'controlled_qube.urdf.xacro')
+    )
+    robot_description = {'robot_description': urdf.toxml()}
+
+    # Start qube_driver.launch.py fra qube_driver-pakken
+    qube_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(qube_driver_pkg, 'launch', 'qube_driver.launch.py')
+        )
+    )
+
+    # Robot State Publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[robot_description]
+    )
+
+    # RViz
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+    )
+
+    return LaunchDescription([
+        qube_driver_launch,
+        robot_state_publisher,
+        rviz,
+    ])
